78ee9b1306750d5c4a55aaaab829887b2981c049
nandorocker <talk@nan.do>
2024-09-12 00:57:19 +0200

    Adds SASS support

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addWatchTarget("./src/sass/");
+
+    eleventyConfig.addPassthroughCopy("./src/sass/");
+
     return {
         dir: {
             input: "src",
