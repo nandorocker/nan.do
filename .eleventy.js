7f471a95fb8fdc4c11d6c40cb8f3e2581c422f23
nandorocker <talk@nan.do>
2024-10-11 22:12:18 +0200

    Switches to 11ty dev server

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,23 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addWatchTarget("src/assets/css/*");
+  // Add custom watch targets for CSS and config files
+  eleventyConfig.addWatchTarget("src/assets/css/tailwind.css");
   eleventyConfig.addWatchTarget("tailwind.config.js");
-  eleventyConfig.addPassthroughCopy({ "src/assets/js/*": "assets/js" });
+
+  // Passthrough copy for assets (JS, images, fonts)
+  eleventyConfig.addPassthroughCopy({ "src/assets/js/*": "." });
+  eleventyConfig.addPassthroughCopy({ "src/assets/images/*": "assets/images" });
+  eleventyConfig.addPassthroughCopy({ "src/assets/fonts/*": "assets/fonts" });
+
+  // Optional advanced chokidar setup
+  eleventyConfig.setServerOptions({
+    liveReload: true,
+    domDiff: true,
+    watch: ["src/**/*.css", "src/**/*.js"], // Adjust according to the paths to watch
+    open: true, // Automatically open the browser
+    port: 8080, // Use a specific port for development
+    showAllHosts: true, // For more flexibility in dev environments
+    indexFileName: "index.html",
+  });
 
   return {
     dir: {
