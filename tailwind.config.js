ef32a1ecfeefa4ac62dcdcb34ffec0c847d53730
nandorocker <talk@nan.do>
2024-10-23 22:50:39 +0200

    Adds lots of content and styles to home

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,6 @@ module.exports = {
   theme: {
     fontFamily: {
       sans: ["Work Sans", "regular"],
-      serif: ["Merriweather", "regular"],
-      slab: ["Montserrat Alternates", "black"],
     },
     extend: {
       colors: {
@@ -16,6 +14,36 @@ module.exports = {
         cream: "#fff6ee",
         lime: "#d6dc82",
         cherry: "#d86072",
+      },
+      borderWidth: {
+        6: "6px",
+      },
+      borderRadius: {
+        xl: "1rem",
+      },
+      height: {
+        "10vh": "10vh",
+        "20vh": "20vh",
+        "30vh": "30vh",
+        "40vh": "40vh",
+        "50vh": "50vh",
+        "60vh": "60vh",
+        "70vh": "70vh",
+        "80vh": "80vh",
+        "90vh": "90vh",
+        "100vh": "100vh",
+      },
+      minHeight: {
+        "10vh": "10vh",
+        "20vh": "20vh",
+        "30vh": "30vh",
+        "40vh": "40vh",
+        "50vh": "50vh",
+        "60vh": "60vh",
+        "70vh": "70vh",
+        "80vh": "80vh",
+        "90vh": "90vh",
+        "100vh": "100vh",
       },
     },
   },
