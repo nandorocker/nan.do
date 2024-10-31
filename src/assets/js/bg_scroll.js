0a7a3c249563ae4cda809105f06515cbacf277d5
nandorocker <talk@nan.do>
2024-11-01 00:50:59 +0100

    Fixes scroll color script footer bug

--- src/assets/js/bg_scroll.js
+++ src/assets/js/bg_scroll.js
@@ -1,22 +1,30 @@
 class BgScroll {
   constructor(element) {
     this.element = element;
-    this.sections = this.element.querySelectorAll("section");
+    this.sections = this.element.querySelectorAll("section, footer");
     this.observeSection = this.observeSection.bind(this);
     this.init();
   }
 
   observeSection(entries) {
     entries.forEach((entry) => {
-      if (entry.isIntersecting) {
+      // console.log(
+      //   "Observed entry:",
+      //   entry.target.id,
+      //   "isIntersecting:",
+      //   entry.isIntersecting
+      // );
+      if (entry.isIntersecting && entry.target.dataset.bgClass) {
         // Remove all bg classes
         this.element.classList.forEach((className) => {
           if (className.startsWith("bg-")) {
             this.element.classList.remove(className);
+            // console.log("Added class:", entry.target.dataset.bgClass);
           }
         });
         // Add the proper bg class
         this.element.classList.add(entry.target.dataset.bgClass);
+        // console.log("Added class:", entry.target.dataset.bgClass);
       }
     });
   }
