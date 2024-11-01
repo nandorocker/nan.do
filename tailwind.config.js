77ef5b054403e0e3af139af7563dc08f5f6a620d
nandorocker <talk@nan.do>
2024-11-01 04:50:08 +0100

    Adds shades to color palette
    Thanks ChatGPT!!!!

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,66 @@ module.exports = {
     },
     extend: {
       colors: {
-        lavender: "#c6c2f2",
-        obsidian: "#262628",
-        cream: "#fff6ee",
-        lime: "#d6dc82",
-        cherry: "#d86072",
+        lavender: {
+          DEFAULT: "#c6c2f2",
+          100: "#eceaf9",
+          200: "#d9d7f4",
+          300: "#c6c2f2", // same as DEFAULT
+          400: "#a6a2e5",
+          500: "#8b86da",
+          600: "#7e7bd1",
+          700: "#5c5bad",
+          800: "#45438a",
+          900: "#312f63",
+        },
+        obsidian: {
+          DEFAULT: "#262628",
+          100: "#f3f3f3",
+          200: "#cfcfd1",
+          300: "#a5a5a9",
+          400: "#7c7c81",
+          500: "#525259",
+          600: "#3d3d42",
+          700: "#262628", // same as DEFAULT
+          800: "#1b1b1c",
+          900: "#0f0f10",
+        },
+        cream: {
+          DEFAULT: "#fff6ee",
+          100: "#fffaf7",
+          200: "#ffefe2",
+          300: "#ffe4cf",
+          400: "#ffd9b9",
+          500: "#fff6ee", // same as DEFAULT
+          600: "#f5d4a7",
+          700: "#e1a25e",
+          800: "#c7834b",
+          900: "#a56a3b",
+        },
+        lime: {
+          DEFAULT: "#d6dc82",
+          100: "#f5f7e4",
+          200: "#ecf0c8",
+          300: "#e0e6a9",
+          400: "#ccd474",
+          500: "#d6dc82", // same as DEFAULT
+          600: "#b1b866",
+          700: "#8e964c",
+          800: "#616d22",
+          900: "#4e591a",
+        },
+        cherry: {
+          DEFAULT: "#d86072",
+          100: "#f7e8ea",
+          200: "#f1c4c9",
+          300: "#e7989d",
+          400: "#e16e7b",
+          500: "#d86072", // same as DEFAULT
+          600: "#ba4859",
+          700: "#943944",
+          800: "#6c2930",
+          900: "#4f1d23",
+        },
       },
       borderWidth: {
         6: "6px",
