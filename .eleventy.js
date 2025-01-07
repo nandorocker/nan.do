2b765439f6a367037e786e31b00e58d521dbfcc9
nandorocker <talk@nan.do>
2025-01-07 15:48:40 +0100

    Moves taos to local

--- .eleventy.js
+++ .eleventy.js
@@ -14,6 +14,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/images/**/*");
   eleventyConfig.addPassthroughCopy("src/assets/fonts/*");
   eleventyConfig.addPassthroughCopy("src/favicon.png");
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/taos/dist/taos.js": "taos.js",
+    "node_modules/taos/dist/taos.js.map": "taos.js.map",
+  });
 
   // Optional advanced chokidar setup
   eleventyConfig.setServerOptions({
